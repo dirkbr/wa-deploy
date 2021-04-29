d65c64ea70e40363cb8030a5c5c54091164a663c
dirkbr <guppy.dirk@web.de>
2021-04-29 13:54:09 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,5 +3,5 @@
 git clone https://github.com/thecodingmachine/workadventure.git /opt/workadventure
 git clone https://github.com/dirkbr/wa-deployment.git /opt/wa-deployment
 
-mv --backup /opt/wa-deployment/.env /opt/workadventure/.env
-mv --backup /opt/wa-deployment/docker-compose.yaml /opt/workadventure/docker-compose.yaml
+cp --backup /opt/wa-deployment/.env /opt/workadventure/.env
+cp --backup /opt/wa-deployment/docker-compose.yaml /opt/workadventure/docker-compose.yaml
