34e92bcfcf20f2e08d854a5756cb55dcaa59979b
dirkbr <guppy.dirk@web.de>
2021-05-02 18:46:18 +0200

    minor changes

--- install-docker.sh
+++ install-docker.sh
@@ -8,7 +8,10 @@ fi
 
 apt update
 
+# prerequisites
 apt install apt-transport-https ca-certificates curl gnupg lsb-release
+
+# docker installation
 curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo \
   "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
@@ -17,6 +20,6 @@ echo \
 apt update
 apt install docker-ce docker-ce-cli containerd.io
 
-# docker-compose
+# docker-compose installation
 curl -L "https://github.com/docker/compose/releases/download/1.29.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
