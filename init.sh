4255bbe5332d7d961cd1641552edd3819b49e671
dirkbr <guppy.dirk@web.de>
2021-05-11 15:21:24 +0200

    minor changes

--- init.sh
+++ init.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 WA_ROOT=/opt/workadventure
 
-date > ~/user-data-time.txt
-
 ## create user workadventure with Home-Directory /opt/workadventure (vgl. binary kitch)
 ##
 
@@ -14,22 +12,17 @@ DEBIAN_FRONTEND=noninteractive apt --assume-yes install git nano htop #nginx
 git clone https://github.com/thecodingmachine/workadventure.git ${WA_ROOT}/source
 #git clone https://git.binary-kitchen.de/noby/workadventure.git ${WA_ROOT}/source
 git clone https://github.com/dirkbr/wa-deployment.git ${WA_ROOT}/wa-deployment
-/opt/wa-deployment/install-docker.sh
+bash ${WA_ROOT}/install-docker.sh
 
 # copy docker  files
 cp --backup ${WA_ROOT}/wa-deployment/.env ${WA_ROOT}/source/.env
 cp --backup ${WA_ROOT}/wa-deployment/docker-compose.yaml ${WA_ROOT}/source/docker-compose.yaml
+cp --backup ${WA_ROOT}/wa-deployment/.bashrc ~/.bashrc
 
 # copy nginx config
 #cp ${WA_ROOT}/wa-deployment/workadventure /etc/nginx/sites-available/workadventure
 #ln -s /etc/nginx/sites-available/workadventure /etc/nginx/sites-enabled/workadventure
 #mkdir /etc/nginx/ssl
-
-# increase server_names_hash_bucket_size to 64 in /etc/nginx/nginx.conf
-
-
-
-date >> ~/user-data-time.txt
 
 ### afterwards:
 
